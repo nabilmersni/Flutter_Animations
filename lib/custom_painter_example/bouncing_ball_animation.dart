cb778f32ba8c350c721c748a5fe999f06ec1fee3
nabilmersni <nabilmersni123@gmail.com>
2024-12-21 19:51:08 +0100

    Animate a custom Painter

--- lib/custom_painter_example/bouncing_ball_animation.dart
+++ lib/custom_painter_example/bouncing_ball_animation.dart
@@ -7,7 +7,35 @@ class BouncingBallAnimation extends StatefulWidget {
   State<BouncingBallAnimation> createState() => _BouncingBallAnimationState();
 }
 
-class _BouncingBallAnimationState extends State<BouncingBallAnimation> {
+class _BouncingBallAnimationState extends State<BouncingBallAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    );
+    animation = Tween<double>(begin: 0.1, end: 1).animate(controller);
+
+    controller.forward();
+
+    animation.addListener(
+      () {
+        if (animation.isCompleted) {
+          controller.reverse();
+        }
+
+        if (!animation.isAnimating) {
+          controller.forward();
+        }
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,10 +43,36 @@ class _BouncingBallAnimationState extends State<BouncingBallAnimation> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            // Need Ball
+            AnimatedBuilder(
+                animation: animation,
+                builder: (context, child) {
+                  return CustomPaint(
+                    size: const Size(200, 200),
+                    painter: BouncingBallPainter(animation: animation.value),
+                  );
+                })
           ],
         ),
       ),
     );
+  }
+}
+
+class BouncingBallPainter extends CustomPainter {
+  final double animation;
+
+  BouncingBallPainter({super.repaint, required this.animation});
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.drawCircle(
+      Offset(size.width / 2, size.height - (size.height * animation)),
+      20,
+      Paint()..color = Colors.blue,
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
